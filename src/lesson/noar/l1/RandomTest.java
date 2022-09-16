2ed6b39afdbac219ce407a141558691df045f13a
Kazunori Hisada <dev100kg@outlook.jp>
2022-09-16 12:18:35 +0900

    乱数範囲の変更

--- src/lesson/noar/l1/RandomTest.java
+++ src/lesson/noar/l1/RandomTest.java
@@ -9,9 +9,10 @@ public class RandomTest {
 	public static void main(String[] args) {
 		ArrayList<Integer> a = new ArrayList<>();
 		for (int i = 0; i < 10000; i++) {
-			Random rnd = new Random();
-			int randomCallTime = rnd.nextInt(8) + 1;
-			a.add(randomCallTime);
+			Random r = new Random();
+			//TODO:範囲指定のやり方検討
+			int num = r.nextInt(7);
+			a.add(num);
 		}
 		System.out.println(a.stream().max(Comparator.naturalOrder()).get());
 		System.out.println(a.stream().min(Comparator.naturalOrder()).get());
