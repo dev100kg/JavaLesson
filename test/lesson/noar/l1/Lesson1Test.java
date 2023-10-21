f83c0b2bc766f11cece5ed6ad5c073b74093bc7a
Kazunori Hisada <dev100kg@outlook.jp>
2023-10-21 15:50:22 +0900

    テストコード

--- test/lesson/noar/l1/Lesson1Test.java
+++ test/lesson/noar/l1/Lesson1Test.java
@@ -1,19 +1,59 @@
 package lesson.noar.l1;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class Lesson1Test {
-    @Test
+    private PrintStream defaultPrintStream;
+    private ByteArrayOutputStream byteArrayOutputStream;
+
+    @BeforeEach
+    public void setUp() {
+        defaultPrintStream = System.out;
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(new BufferedOutputStream(byteArrayOutputStream)));
+    }
+
+    @RepeatedTest(100)
     void getCryTimeTest() throws NoSuchMethodException, SecurityException, IllegalAccessException,
             IllegalArgumentException, InvocationTargetException {
-        for (int i = 0; i < 100000; i++) {
-            int act = (int) this.doStaticPrivateMethod("getCryTime", null, null);
-            assertTrue(act >= 1 && act <= 8);
-        }
+        int actual = (int) this.doStaticPrivateMethod("getCryTime", null, null);
+        assertTrue(actual >= 1 && actual <= 8);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1,2,3,4,5,6,7,8 })
+    void getCicadaCryTest(int i) throws NoSuchMethodException, SecurityException, IllegalAccessException,
+            IllegalArgumentException, InvocationTargetException {
+            String actual = (String) this.doStaticPrivateMethod("getCicadaCry", new Class[] { int.class },
+                    new Object[] { i });
+            assertEquals("みーんみーん" + "みん".repeat(i) + "みーん", actual);
+    }
+
+    @Test
+    void mainTest() {
+        Lesson1.main(new String[] {});
+        System.out.flush();
+        final String actual = byteArrayOutputStream.toString();
+        assertTrue(actual.matches("(みーん){2}(みん){1,8}みーん\\n".repeat(5)));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(defaultPrintStream);
     }
 
     private Object doStaticPrivateMethod(String name, Class<?>[] types, Object[] args) throws NoSuchMethodException,
