e9424f5e52771994bc6d175736c1fac92d5ee971
Kazunori Hisada <dev100kg@outlook.jp>
2023-10-22 23:03:35 +0900

    メソッド切り出し

--- src/lesson/noar/l2/Lesson2.java
+++ src/lesson/noar/l2/Lesson2.java
@@ -5,22 +5,33 @@ public class Lesson2 {
 	public static void main(String[] args) {
 		int i = 11;
 		while (true) {
-			String strInt = String.valueOf(i);
-			String rStrInt = new StringBuffer(strInt).reverse().toString();
-			if (strInt.equals(rStrInt)) {
-				String strBin = Integer.toBinaryString(i);
-				String rStrBin = new StringBuffer(strBin).reverse().toString();
-				if (strBin.equals(rStrBin)) {
-					String strOct = Integer.toOctalString(i);
-					String rStrOct = new StringBuffer(strOct).reverse().toString();
-					if (strOct.equals(rStrOct)) {
-						System.out.println(i);
-						break;
-					}
-				}
+			if (isDecimalPalindrome(i) && isBinaryPalindrome(i) && isOctalPalindrome(i)) {
+				System.out.println(i);
+				break;
 			}
 			i += 2;
 		}
+	}
+
+
+	private static boolean isDecimalPalindrome(int num) {
+		String strDec = String.valueOf(num);
+		return strDec.equals(reverseString(strDec));
+	}
+
+	private static boolean isBinaryPalindrome(int num) {
+		String strBin = Integer.toBinaryString(num);
+		return strBin.equals(reverseString(strBin));
+	}
+
+
+	private static boolean isOctalPalindrome(int num) {
+		String strOct = Integer.toOctalString(num);
+		return strOct.equals(reverseString(strOct));
+	}
+
+	private static String reverseString(String strInt) {
+		return new StringBuffer(strInt).reverse().toString();
 	}
 
 }
